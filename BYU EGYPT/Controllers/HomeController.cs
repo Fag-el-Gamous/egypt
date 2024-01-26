1edeeff9c9b87490ce864c1a9197513f6a9db9b7
LIAMYUZEDONG <yyyzzddd@gmail.com>
2024-01-26 11:33:12 -0700

    new code
    lol

---
+++ BYU EGYPT/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BYU_EGYPT.Models;
+
+namespace BYU_EGYPT.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Explore()
+    {
+        return View();
+    }
+
+    public IActionResult Search()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ BYU EGYPT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BYU_EGYPT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
